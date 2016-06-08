13cf257e70982b89070aabe44acd51fa2c407dfa
darkness24713 <msmitzel@gmail.com>
2016-06-08 17:50:15 -0400

    testing change

--- src/com/training/opengl/Tutorial.java
+++ src/com/training/opengl/Tutorial.java
@@ -3,6 +3,6 @@ package com.training.opengl;
 public class Tutorial {
 
 	public static void main(String[] arg){
-		System.out.println("hello Today");
+		System.out.println("hello Today SUP");
 	}
 }
