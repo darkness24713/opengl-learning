d8c23eb6f30ca67d107c90acae50af2f93f052b6
darkness24713 <msmitzel@gmail.com>
2016-06-08 19:12:33 -0400

    asdf

--- src/com/training/opengl/Tutorial.java
+++ src/com/training/opengl/Tutorial.java
@@ -3,6 +3,6 @@ package com.training.opengl;
 public class Tutorial {
 
 	public static void main(String[] arg){
-		System.out.println("hello Today teghjlkkkkkkkkkkkksting this sddf");
+		System.out.println("hello Today teghjlkkkkkkkkkkkksting sdfasadfthis sddf");
 	}
 }
