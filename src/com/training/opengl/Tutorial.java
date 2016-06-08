7877184d5daa44354359f22d1ffc0e884f447e3f
darkness24713 <msmitzel@gmail.com>
2016-06-08 19:14:22 -0400

    Testings again

--- src/com/training/opengl/Tutorial.java
+++ src/com/training/opengl/Tutorial.java
@@ -3,6 +3,6 @@ package com.training.opengl;
 public class Tutorial {
 
 	public static void main(String[] arg){
-		System.out.println("hello Today teghjlkkkkkkkkkkkksting sdfasadfthis sddf");
+		System.out.println("hello Today teghjlkkkkkkkkkkkkstinkljhlk;jhg sdfasadfthis sddf");
 	}
 }
