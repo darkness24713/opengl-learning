508bdca4d9eefd2c979046aa5746e3bbabec86dc
darkness24713 <msmitzel@gmail.com>
2016-06-08 19:31:37 -0400

    Added hello world

--- src/com/training/opengl/Tutorial.java
+++ src/com/training/opengl/Tutorial.java
@@ -2,7 +2,5 @@ package com.training.opengl;
 
 public class Tutorial {
 
-	public static void main(String[] arg){
-		System.out.println("hello Today teghjlkkkkkkkkkkkkstinkljhlk;jhg sdfasadfthis sddf");
-	}
+	
 }
